7e924ec84900de4d7dfa45c97e812b9fbca582ff
Hannah Crocker <hannahcrocker@Hannahs-Air.roc.mn.charter.com>
2018-02-15 12:01:36 -0600

    Exercise 7

--- src/conditions_loops/labs/Exercise_07.java
+++ src/conditions_loops/labs/Exercise_07.java
@@ -1,12 +1,43 @@
 package conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
- *      Take in a word from the user and usig a "while" loop, find the first vowel in the word.
+ *      Take in a word from the user and using a "while" loop, find the first vowel in the word.
  *      Once you find the vowel, print out the word and the first vowel.
  *
  */
 
 public class Exercise_07 {
+
+    public static void main(String[] args) {
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // prompt user
+        System.out.print("Enter any word here: ");
+
+        String word = scanner.nextLine();
+
+        boolean found = false;
+        char letter = word.charAt(0);
+        int count = 0;
+
+        while (!found) {
+            letter = word.charAt(count);
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+                System.out.println("Your word is: " + word);
+                System.out.println("The first vowel in your word is: " + letter);
+                found = true;
+            }else{
+                count++;
+            }
+
+
+        }
+
+    }
 }
+

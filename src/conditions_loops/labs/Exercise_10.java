454b798daa1339c7d3b8622b762237ed294acbbc
Hannah Crocker <hannahcrocker@Hannahs-Air.roc.mn.charter.com>
2018-02-15 12:41:47 -0600

    Exercise 10

--- src/conditions_loops/labs/Exercise_10.java
+++ src/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,17 @@ package conditions_loops.labs;
  */
 
 public class Exercise_10 {
+
+    public static void main(String[] args) {
+
+        int x;
+
+        //print odd numbers from 0 to 100
+        for (x = 0; x <= 100; x++) {
+            if ( (x % 2) == 0) continue;
+            System.out.println(x);
+        }
+
+    }
+
 }

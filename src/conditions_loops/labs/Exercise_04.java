1bc8895a1aa8634247882b5653f9a2eb4da9edbe
Hannah Crocker <hannahcrocker@Hannahs-Air.roc.mn.charter.com>
2018-02-14 20:33:31 -0600

    updated

--- src/conditions_loops/labs/Exercise_04.java
+++ src/conditions_loops/labs/Exercise_04.java
@@ -8,4 +8,15 @@ package conditions_loops.labs;
  */
 
 public class Exercise_04 {
+
+    public static void main(String[] args) {
+
+        //display all even numbers from 1-100
+        for (int x = 0; x < 101; x += 2) {
+            System.out.println(x);
+        }
+
+
+        }
+
 }

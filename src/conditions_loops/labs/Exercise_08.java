409ffccf2483f083c287d808025b9ef31403f33d
Hannah Crocker <hannahcrocker@Hannahs-Air.roc.mn.charter.com>
2018-02-15 12:18:59 -0600

    Exercise 8

--- src/conditions_loops/labs/Exercise_08.java
+++ src/conditions_loops/labs/Exercise_08.java
@@ -8,4 +8,16 @@ package conditions_loops.labs;
  */
 
 public class Exercise_08 {
+
+    public static void main(String[] args) {
+
+        //start with the top of the count
+        int x = 1003;
+
+        do {
+            System.out.println(x -= 3);
+
+        } while (x >= 3) ;
+
+    }
 }

e0cdb8d323a7a8b7e948ba1b12093c257ac7eb50
Hannah Crocker <hannahcrocker@Hannahs-Air.roc.mn.charter.com>
2018-02-14 20:18:37 -0600

    updated conditions_loops Exercises 1 and 2

--- src/conditions_loops/labs/Exercise_01.java
+++ src/conditions_loops/labs/Exercise_01.java
@@ -23,7 +23,12 @@ public class Exercise_01 {
         // assign input to variable as int
         int number = scanner.nextInt();
 
-        // write completed code here
+        //determine if number entered is even or odd using modulus
+        if (number % 2 == 0) {
+            System.out.println("this number is even");
+        } else {
+            System.out.println("this number is odd");
 
+        }
     }
 }
